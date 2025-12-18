b6d78f6f34411b5edf2c4ef820548537e0354dee
sven-friedel <svenfriedel@outlook.com>
2025-12-18 09:17:19 +0900

    removed unnecessary for loop in cards data cleaning python script

--- Assets/Script/cards_data_cleaning.py
+++ Assets/Script/cards_data_cleaning.py
@@ -2,12 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('Assets/Dataset/Raw_Dataset/cards_data.csv')
 
-# Clean the currency columns
-cols_to_clean = ['credit_limit']
-
-for col in cols_to_clean:
-    # Remove $ and commas, then convert to numeric
-    df[col] = df[col].replace(r'[\$,]', '', regex=True).astype(float)
+# Remove $ and commas, then convert to numeric
+df['credit_limit'] = df['credit_limit'].replace(r'[\$,]', '', regex=True).astype(float)
 
 # Clean Date: Convert 09/2002 to 2002-09-01
 df['expires'] = pd.to_datetime(df['acct_open_date'], format='%m/%Y').dt.strftime('%Y-%m-%d')
